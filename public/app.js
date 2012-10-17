dd3e3f080ebcfad8683406bbe5a4e15bdab69ee6
Michael Huneke <mhuneke@gmail.com>
2012-10-17 14:35:10 -0400

    add evergreen photos, thumbnails and collection view

--- public/app.js
+++ public/app.js
@@ -4,7 +4,15 @@
   var app = angular.module('app', ['app.controllers']);
 
   app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/', {templateUrl: '/partials/home.html'});
+    $routeProvider.when('/', {templateUrl: '/partials/home.html', controller: 'HomeCtrl'});
+    $routeProvider.when('/collection/:id', {templateUrl: '/partials/view-collection.html', controller: 'CollectionCtrl'});
+    $routeProvider.when('/collection/:id/:imageId', {templateUrl: '/partials/view-collection.html', controller: 'CollectionCtrl'});
+    $routeProvider.when('/thumbnails', {templateUrl: '/partials/view-thumbnails.html', controller: 'ThumbnailsCtrl'});
+
+    $routeProvider.when('/about', {templateUrl: '/partials/about.html'});
+    $routeProvider.when('/contact', {templateUrl: '/partials/contact.html'});
+
+    $routeProvider.otherwise({redirectTo: '/'});
   }]);
 
 }(angular));
--- public/controllers.js
+++ public/controllers.js
@@ -1,10 +1,74 @@
-(function(angular) {
+(function(angular, $, _) {
   'use strict';
 
   var controllers = angular.module('app.controllers', []);
 
-  controllers.controller('HomeController', [function() {
+  controllers.controller('HomeCtrl', ['$scope', '$log', function($scope, $log) {
+    // Setup backstretch when you land
+    setTimeout(function() {
+      $scope.backstretch = $.backstretch(["/images/astoria.jpg", "/images/aspen.jpg"], {fade: 500, duration: 4000});
+    }, 1000);
 
+    // Kill backstretch when you change routes from home
+    $scope.$on('$routeChangeStart', function(route) {
+      $log.log(route);
+      $scope.backstretch.destroy();
+    });
   }]);
-}(angular));
-  
+
+  controllers.controller('ThumbnailsCtrl', ['$scope', '$log', '$http', function($scope, $log, $http) {
+    $scope.collections = [];
+    $http.get('/json/evergreen.json').success(function(data) {
+      $scope.collections.push(data);
+    });
+  }]);
+
+  controllers.controller('CollectionCtrl', ['$scope', '$routeParams', '$log', '$http', '$location', function($scope, $routeParams, $log, $http, $location) {
+    $log.log($routeParams);
+    $http.get('/json/' + $routeParams.id + ".json").success(function(data) {
+      $scope.collection = data;
+      $scope.photoUrls = _.map($scope.collection.photos, function(photo) {
+        return photo.image;
+      });
+      $scope.thumbnailUrls = _.map($scope.collection.photos, function(photo) {
+        return photo.thumbnail;
+      });
+
+      $('#collection-image-preview').backstretch($scope.photoUrls, {fade: 500, duration: 4000});
+      $('#collection-image-preview').data('backstretch').pause();
+
+      $scope.$watch('currentImageIndex', function() {
+        $('#collection-image-preview').data('backstretch').show($scope.currentImageIndex);
+        $scope.currentImageTitle = $scope.collection.photos[$scope.currentImageIndex].title;
+      });
+
+      $scope.currentImageIndex = $routeParams.imageId || 0;
+    });
+
+    $scope.nextImage = function() {
+      if ($scope.currentImageIndex === ($scope.photoUrls.length - 1)) {
+        return;
+      }
+      $scope.currentImageIndex++;
+    };
+
+    $scope.previousImage = function() {
+      if ($scope.currentImageIndex === 0) {
+        return;
+      }
+      $scope.currentImageIndex--;
+    };
+
+    $scope.playSlideshow = function() {
+      $('#collection-image-preview').data('backstretch').resume();
+    };
+
+    $scope.pauseSlideshow = function() {
+      $('#collection-image-preview').data('backstretch').pause();
+    };
+
+    $scope.gotoImage = function(index) {
+      $scope.currentImageIndex = index;
+    };
+  }]);
+}(angular, $, _));
